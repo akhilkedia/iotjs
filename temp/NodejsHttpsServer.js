64d4ebb762739a9070ce789acd60bf2935635285
Akhil <akhil.kedia@samsung.com>
2017-05-29 11:44:21 +0900

    Simple coding style modifications
    IoT.js-DCO-1.0-Signed-off-by: Akhil akhil.kedia@samsung.com

--- temp/NodejsHttpsServer.js
+++ temp/NodejsHttpsServer.js
@@ -3,22 +3,22 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-	key: fs.readFileSync('./temp/certs/server.key'),
-	cert: fs.readFileSync('./temp/certs/server.pem'),
-	// This is necessary only if using the client certificate authentication.
-	requestCert: true,
-	// This is necessary only if the client uses the self-signed certificate.
-	ca: fs.readFileSync('./temp/certs/ca.pem')
+  key: fs.readFileSync('./temp/certs/server.key'),
+  cert: fs.readFileSync('./temp/certs/server.pem'),
+  // This is necessary only if using the client certificate authentication.
+  requestCert: true,
+  // This is necessary only if the client uses the self-signed certificate.
+  ca: fs.readFileSync('./temp/certs/ca.pem')
 };
 
-var server = https.createServer(options, function (req, res) {
-	var body = 'Hello World!!!!! :D :D :D \n';
-	console.log("Received a request!");
-	res.writeHead(200, { 'Connection' : 'close', 'Content-Length' : body.length });
-	res.end(body);
-	console.log("Sent Response!");
+var server = https.createServer(options, function(req, res) {
+  var body = 'Hello World!!!!! :D :D :D \n';
+  console.log('Received a request!');
+  res.writeHead(200, {'Connection': 'close', 'Content-Length': body.length});
+  res.end(body);
+  console.log('Sent Response!');
 });
 
 server.listen(4443, () => {
-	console.log('Server Started');
+  console.log('Server Started');
 });
--- temp/akhil-test.js
+++ temp/akhil-test.js
@@ -9,119 +9,117 @@ var timers = require('timers');
 
 console.log('akhil-test begin');
 
-
 //************Request 1 ************
 options = {
-	method: 'GET',
-//	host: "eu.httpbin.org",
-	host: "localhost",
-	path: "/",
-	port: 4443,
-	headers : {"Content-Type": "application/json", "cache-control": "no-cache"},
-	ca: "./temp/certs/ca.pem",
-	cert: "./temp/certs/client.pem",
-	key: "./temp/certs/client.key"
+  method: 'GET',
+  //	host: "eu.httpbin.org",
+  host: 'localhost',
+  path: '/',
+  port: 4443,
+  headers: {'Content-Type': 'application/json', 'cache-control': 'no-cache'},
+  ca: './temp/certs/ca.pem',
+  cert: './temp/certs/client.pem',
+  key: './temp/certs/client.key'
 };
 
-var res_body = '';
+var resBody = '';
 var getResponseHandler = function(res) {
-	console.log("Got RESPONSE Callback!");
+  console.log('Got RESPONSE Callback!');
 
-	res.on('data', function(chunk) {
-		console.log("Got Data Callback!");
-		console.log(chunk);
-		res_body += chunk.toString();
-	});
+  res.on('data', function(chunk) {
+    console.log('Got Data Callback!');
+    console.log(chunk);
+    resBody += chunk.toString();
+  });
 
-	res.on('end', function() {
-		console.log("--------In End handler--------");
-		console.log(res_body);
-		console.log("--------Out End handler--------");
-	});
+  res.on('end', function() {
+    console.log('--------In End handler--------');
+    console.log(resBody);
+    console.log('--------Out End handler--------');
+  });
 };
 
 var req1 = https.request(options, getResponseHandler);
 //Note - END MUST BE CALLED!!!!!!
 req1.end();
 
-
 //************ Request 2 ************
-
 options2 = {
-	method: 'POST',
-	protocol: 'https:',
-	host: "api.artik.cloud",
-	path: "/v1.1/devices?Authorization=bearer%202ccf9479a6584522832de388c82320fc",
-	//host : "http://httpbin.org/post",
-	port: 443,
-	highwatermark: 256,
-	headers : {"Content-Length": 164, "Content-Type": "application/json", "cache-control": "no-cache"}
+  method: 'POST',
+  protocol: 'https:',
+  host: 'api.artik.cloud',
+  path: '/v1.1/devices?Authorization=bearer%202ccf9479a6584522832de388c82320fc',
+  //host : "http://httpbin.org/post",
+  port: 443,
+  highwatermark: 256,
+  headers: {'Content-Length': 164, 'Content-Type': 'application/json',
+    'cache-control': 'no-cache'}
 };
-var res_body2 = '';
+var resBody2 = '';
 
 var getResponseHandler2 = function(res) {
-	console.log("Got RESPONSE Callback!!!!!!");
-	console.log(" ------- About to print header ------- ");
-	for (var key in res.headers) {
-		if (res.headers.hasOwnProperty(key)) {
-			console.log(key + " -> " + res.headers[key]);
-		}
-	}
-	console.log(" -------  End print header ------- ");
-	console.log("The statusCode is " + res.statusCode);
-	console.log("The statusMessage is " + res.statusMessage);
-	//res.setTimeout(500);
+  console.log('Got RESPONSE Callback!!!!!!');
+  console.log(' ------- About to print header ------- ');
+  for (var key in res.headers) {
+    if (res.headers.hasOwnProperty(key)) {
+      console.log(key + ' -> ' + res.headers[key]);
+    }
+  }
+  console.log(' -------  End print header ------- ');
+  console.log('The statusCode is ' + res.statusCode);
+  console.log('The statusMessage is ' + res.statusMessage);
+  //res.setTimeout(500);
 
-	res.on('data', function(chunk) {
-		console.log("Got Data Callback!!!!!!");
-		console.log("Will it crash here?");
-		console.log(chunk);
-		res_body2 += chunk.toString();
-		console.log("Toldja");
-		res.pause();
-		var isPaused = res.isPaused();
-		console.log('Current state of stream is  - ' + isPaused);
-	});
+  res.on('data', function(chunk) {
+    console.log('Got Data Callback!!!!!!');
+    console.log('Will it crash here?');
+    console.log(chunk);
+    resBody2 += chunk.toString();
+    console.log('Toldja');
+    res.pause();
+    var isPaused = res.isPaused();
+    console.log('Current state of stream is  - ' + isPaused);
+  });
 
-	res.on('readable', function() {
-		var chunk;
-		console.log('----Got Readable Callback-----');
-		while (null !== (chunk = res.read())) {
-			console.log(chunk);
-			res_body2 += chunk.toString();
-		}
-		console.log('----End Readable Callback-----');
-		res.resume();
-		var isPaused = res.isPaused();
-		console.log('Current state of stream is  - ' + isPaused);
-	});
-	res.on('end', function() {
-		console.log("--------In End handler--------");
-		console.log(res_body2);
-		console.log("--------Out End handler--------");
-	});
-	res.on('close', function() {
-		console.log("--------CONNECTION CLOSED--------");
-	});
-	res.on('error', function(err) {
-		console.log("Got error" + err);
-	});
-	//TODO: Will need to check in timeout
-	//res.on('timeout', function() {
-	//	console.log("Got TIMEOUT!!!");
-	//});
+  res.on('readable', function() {
+    var chunk;
+    console.log('----Got Readable Callback-----');
+    while (null !== (chunk = res.read())) {
+      console.log(chunk);
+      resBody2 += chunk.toString();
+    }
+    console.log('----End Readable Callback-----');
+    res.resume();
+    var isPaused = res.isPaused();
+    console.log('Current state of stream is  - ' + isPaused);
+  });
+  res.on('end', function() {
+    console.log('--------In End handler--------');
+    console.log(resBody2);
+    console.log('--------Out End handler--------');
+  });
+  res.on('close', function() {
+    console.log('--------CONNECTION CLOSED--------');
+  });
+  res.on('error', function(err) {
+    console.log('Got error' + err);
+  });
+  //TODO: Will need to check in timeout
+  //res.on('timeout', function() {
+  //	console.log("Got TIMEOUT!!!");
+  //});
 };
-
 
 var req2 = https.request(options2, getResponseHandler2);
 req2.on('socket', function() {
-	console.log("--------In SOCKET CALLBACK--------");
+  console.log('--------In SOCKET CALLBACK--------');
 });
-req2.write("{\"uid\": \"cb19e910d1cb441ba5a318ea88c3ac23\",\"dtid\": \"dt6b12b870304e4dd5a88de2007f7d171e\",\"name\": \"YourTes22\",\"manifestVersion\": -1,\"manifestVersionPolicy\": \"LATEST\"");
-req2.write("}");
+var msgBody = '{"uid": "cb19e910d1cb441ba5a318ea88c3ac23",' +
+  '"dtid": "dt6b12b870304e4dd5a88de2007f7d171e",' +
+  '"name": "YourTes22","manifestVersion": -1,"manifestVersionPolicy": "LATEST"';
+req2.write(msgBody);
+req2.write('}');
 //Note - END MUST BE CALLED!!!!!!
 req2.end();
-
-
 
 console.log('Hello End');
